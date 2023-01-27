cfd0bdd65fe5e37c5542ab1d9816c9d1e026664d
Vasylyna Lykhogodenko <vasilisapj89@gmail.com>
2023-01-28 01:00:34 +0200

    feat: Added pipe json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,11 +18,15 @@ import { Component } from "@angular/core";
  // styles: ['']
 })
 export class AppComponent {
-  name = 'luis ramirez';
-  imgUrl = 'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
-  currentDate = new Date();
-  cost = 2000;
-  temperature = 25.3;
+  name = 'luis ramirez'
+  imgUrl = 'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0'
+  currentDate = new Date()
+  cost = 2000
+  temperature = 25.3
+  pizza = {
+    toppings: ['pepperoni', 'bacon'],
+    size: 'large'
+  }
 
   getName() {
     return this.name;
